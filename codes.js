8e48f384f528a26b5ad06852570d9fd5c8fb2c2d
Shivam Srivastava <shivamsrivastava9788@gmail.com>
2026-08-13 13:33:06 +0530

    initial commit

---
+++ codes.js
@@ -0,0 +1,130 @@
+const countryList = {
+  "USD": "US", // United States
+  "INR": "IN", // India
+  "EUR": "EU", // Eurozone
+  "GBP": "GB", // United Kingdom
+  "AUD": "AU", // Australia
+  "CAD": "CA", // Canada
+  "SGD": "SG", // Singapore
+  "JPY": "JP", // Japan
+  "CNY": "CN", // China
+  "CHF": "CH", // Switzerland
+  "AED": "AE", // UAE
+  "SAR": "SA", // Saudi Arabia
+  "KRW": "KR", // South Korea
+  "PKR": "PK", // Pakistan
+  "BDT": "BD", // Bangladesh
+  "LKR": "LK", // Sri Lanka
+  "NPR": "NP", // Nepal
+  "THB": "TH", // Thailand
+  "MYR": "MY", // Malaysia
+  "IDR": "ID", // Indonesia
+  "PHP": "PH", // Philippines
+  "ZAR": "ZA", // South Africa
+  "RUB": "RU", // Russia
+  "BRL": "BR", // Brazil
+  "MXN": "MX", // Mexico
+  "TRY": "TR", // Turkey
+  "SEK": "SE", // Sweden
+  "NOK": "NO", // Norway
+  "DKK": "DK", // Denmark
+  "PLN": "PL", // Poland
+  "HKD": "HK", // Hong Kong
+  "NZD": "NZ"  // New Zealand
+};
+
+
+
+
+/*const countryList = {
+    AED: "AE",
+    AFN: "AF",
+    XCD: "AG",
+    AMD: "AM",
+    ANG: "AN",
+    AOA: "AO",
+    AQD: "AQ",
+    ARS: "AR",
+    AUD: "AU",
+    AZN: "AZ",
+    BAM: "BA",
+    BBD: "BB",
+    BDT: "BD",
+    XOF: "BE",
+    BGN: "BG",
+    BHD: "BH",
+    BIF: "BI",
+    BMD: "BM",
+    BND: "BN",
+    BOB: "BO",
+    BRL: "BR",
+    BSD: "BS",
+    NOK: "BV",
+    BWP: "BW",
+    BYR: "BY",
+    BZD: "BZ",
+    CAD: "CA",
+    CDF: "CO",
+    XAF: "CF",
+    CHF: "CH",
+    CLP: "CL",
+    CNY: "CN",
+    COP: "CO",
+    CRC: "CR",
+    CUP: "CU",
+    CVE: "CV",
+    CYP: "CY",
+    CZK: "CZ",
+    INR: "IN",
+
+
+};
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="design.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Currency Converter</title>
+</head>
+<body>
+    <div class="container">
+    <h2>Currency Converter</h2>
+    <form>
+        <div class="amount">
+            <p>Enter Amount</p>
+            <input value="100" type="text"/>
+
+        </div>
+        <div class="dropdown">
+            <div class="from">
+                <p>From</p>
+                <div class="select-container">
+                    <img src="https://flagsapi.com/US/flat/64.png">
+                    <select name="from">
+                    </select>
+                </div>
+            </div>
+            <i class="fa-solid fa-arrow-right-arrow-left"></i>
+            <div class="to">
+                <p>To</p>
+                <div class="select-container">
+                    <img src="https://flagsapi.com/IN/flat/64.png">
+                    <select name="from">
+                    </select>
+                </div>
+            </div>
+            
+        </div>
+        <div class="msg">1 USD = 80 INR</div>
+        <button>Get Exchange Rate</button>
+    </form>
+    </div>
+    <script src="codes.js"></script>
+    <script src="logic.js"></script>
+</body>
+</html>
+*/
